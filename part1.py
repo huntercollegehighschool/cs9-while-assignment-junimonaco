52070cfbc47185b0bb4ffe5c58faac8706fcc163
Juni Monaco <94643589+junimonaco@users.noreply.github.com>
2021-12-12 16:34:33 +0000

    finished parts 1 & 2

--- part1.py
+++ part1.py
@@ -16,6 +16,6 @@ numbertosquare = 1
 numsquares = 0
 while numbertosquare ** 2 <= num:
     numsquares = numsquares + 1
-    numbertosquare ** 2
+    numbertosquare = numbertosquare + 1
 
 print("Number of squares less than or equal to", str(num)+':', numsquares)
