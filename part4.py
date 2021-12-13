20b83a01caa678e2bd90405f826c10ba35d3f2a2
Juni Monaco <94643589+junimonaco@users.noreply.github.com>
2021-12-13 13:08:00 +0000

    started part 4

--- part4.py
+++ part4.py
@@ -12,3 +12,11 @@ Enter a number or enter 0 to stop: 0
 Product: 60
 
 '''
+num = int(input("Enter a number or enter 0 to stop:"))
+prod = num
+
+while num != 0:
+  num = int(input("Enter a number or enter 0 to stop:"))
+  prod = prod * num
+
+print("Product:", prod)
